f1c63838ef6c63cd5075adbe84f1cd6d5e635af6
Ria-2417 <139850805+Ria-2417@users.noreply.github.com>
2024-06-08 17:25:03 +0530

    deploy

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,12 @@
+import PageContent from '../components/PageContent';
+
+function HomePage() {
+  return (
+    <PageContent title="WELCOME!">
+      <p>Good Luck for your future and HAPPY CODING :) </p>
+      {/* <p>Browse all our amazing events!</p> */}
+    </PageContent>
+  );
+}
+
+export default HomePage;
